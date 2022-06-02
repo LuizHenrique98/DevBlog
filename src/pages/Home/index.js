9dd366b10f992f460e0ed11c0f0981a311146b3d
LuLuDev <luiz_henrique_77@hotmail.com>
2022-06-02 01:03:49 -0300

    Final aula 2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
   SafeAreaView,
+  FlatList,
 } from 'react-native';
 
 import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 
+import api from '../../services/api';
+
+import CategoryItem from '../../components/CategoryItem';
+import {getFavorite, setFavorite} from '../../services/favorite';
+import FavoritePosts from '../../components/FavoritePosts';
+
 export default function Home() {
   const navigation = useNavigation();
+  const [categories, setCategories] = useState([]);
+  const [favCategory, setFavCategory] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      const category = await api.get('/api/categories?populate=icon');
+      setCategories(category.data.data);
+    }
+
+    loadData();
+  }, []);
+
+  useEffect(() => {
+    async function favorite() {
+      const response = await getFavorite();
+      setFavCategory(response);
+    }
+    favorite();
+  }, []);
+
+  async function handleFavorite(id) {
+    const response = await setFavorite(id);
+
+    setFavCategory(response);
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -21,6 +53,36 @@ export default function Home() {
         <TouchableOpacity onPress={() => navigation.navigate('Search')}>
           <Text style={styles.icon}>🔍</Text>
         </TouchableOpacity>
+      </View>
+
+      <FlatList
+        horizontal={true}
+        contentContainerStyle={{paddingRight: 12}}
+        style={styles.categories}
+        data={categories}
+        keyExtractor={item => String(item.id)}
+        renderItem={({item}) => (
+          <CategoryItem data={item} favorite={() => handleFavorite(item.id)} />
+        )}
+      />
+
+      <View style={styles.main}>
+        {favCategory.length !== 0 && (
+          <FlatList
+            style={{marginTop: 50, maxHeight: 100, paddingStart: 18}}
+            contentContainerStyle={{paddingEnd: 18}}
+            data={favCategory}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={item => String(item.id)}
+            renderItem={({item}) => <FavoritePosts data={item} />}
+          />
+        )}
+
+        <Text
+          style={[styles.title, {marginTop: favCategory.length > 0 ? 14 : 46}]}>
+          Conteúdos em alta
+        </Text>
       </View>
     </SafeAreaView>
   );
@@ -46,5 +108,24 @@ const styles = StyleSheet.create({
   },
   icon: {
     fontSize: 24,
+  },
+  categories: {
+    maxHeight: 115,
+    backgroundColor: '#EFEFEF',
+    marginHorizontal: 18,
+    borderRadius: 8,
+    zIndex: 9,
+  },
+  main: {
+    backgroundColor: '#FFF',
+    flex: 1,
+    marginTop: -30,
+  },
+  title: {
+    fontSize: 21,
+    paddingHorizontal: 18,
+    marginBottom: 14,
+    fontWeight: 'bold',
+    color: '#162133',
   },
 });
